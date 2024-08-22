99917ae45389353f9060feacfdde505c26549d97
Norikinho <flavia.noriko.fukuda.portfolio@gmail.com>
2024-08-22 10:48:47 -0300

    cleaning not used importations

--- src/main/java/com/br/primeiroprojetodesafio/primeiroprojetodesafio/modules/course/controllers/CourseController.java
+++ src/main/java/com/br/primeiroprojetodesafio/primeiroprojetodesafio/modules/course/controllers/CourseController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.br.primeiroprojetodesafio.primeiroprojetodesafio.CourseEntity;
-import com.br.primeiroprojetodesafio.primeiroprojetodesafio.modules.course.dto.CourseDTO;
 import com.br.primeiroprojetodesafio.primeiroprojetodesafio.modules.course.dto.CreateCourseDTO;
 import com.br.primeiroprojetodesafio.primeiroprojetodesafio.modules.course.useCases.CreateCourseUseCase;
 
