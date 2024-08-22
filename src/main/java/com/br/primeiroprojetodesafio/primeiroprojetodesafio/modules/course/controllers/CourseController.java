7e6382d356417c30c325b5494c68b40995f40522
Norikinho <flavia.noriko.fukuda.portfolio@gmail.com>
2024-08-22 10:46:07 -0300

    Creating first post

---
+++ src/main/java/com/br/primeiroprojetodesafio/primeiroprojetodesafio/modules/course/controllers/CourseController.java
@@ -0,0 +1,35 @@
+package com.br.primeiroprojetodesafio.primeiroprojetodesafio.modules.course.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.br.primeiroprojetodesafio.primeiroprojetodesafio.CourseEntity;
+import com.br.primeiroprojetodesafio.primeiroprojetodesafio.modules.course.dto.CourseDTO;
+import com.br.primeiroprojetodesafio.primeiroprojetodesafio.modules.course.dto.CreateCourseDTO;
+import com.br.primeiroprojetodesafio.primeiroprojetodesafio.modules.course.useCases.CreateCourseUseCase;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
+@RequestMapping("/course")
+public class CourseController {
+    @Autowired
+    private CreateCourseUseCase createCourseUseCase;
+
+    @PostMapping("/")
+    public CourseEntity create(@RequestBody CreateCourseDTO createCourseDTO, HttpServletRequest request) {
+        var courseEntity = CourseEntity.builder()
+        .name(createCourseDTO.getName())
+        .category(createCourseDTO.getCategory())
+        .active(createCourseDTO.getActive())
+        .build();
+        
+        return createCourseUseCase.execute(courseEntity);
+    }
+    
+}
---
+++ src/main/java/com/br/primeiroprojetodesafio/primeiroprojetodesafio/modules/course/dto/CreateCourseDTO.java
@@ -0,0 +1,10 @@
+package com.br.primeiroprojetodesafio.primeiroprojetodesafio.modules.course.dto;
+
+import lombok.Data;
+
+@Data
+public class CreateCourseDTO {
+    private String active;
+    private String name;
+    private String category;
+}
